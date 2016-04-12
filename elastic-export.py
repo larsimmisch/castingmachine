36ba6df8bb6db68e88915b32bd3b2de80a1882f5
Lars Immisch <lars@ibp.de>
2016-04-13 00:47:23 +0200

    Drilling into cm_medias

---
+++ elastic-export.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python2
+
+# MYSQL-python does not support python3
+
+from __future__ import print_function
+
+import MySQLdb as mdb
+import sys
+import json
+from keyring import get_keyring
+from argparse import ArgumentParser
+
+from algoliasearch import algoliasearch
+
+# Cached cm_value_list, in the form id -> value
+cm_values = {}
+
+def truthy(v):
+    # 0 is True
+    if type(v) == int:
+        return True
+
+    # but otherwise use python's concept of truthiness
+    return bool(v)
+
+def check_str_list(l):
+    for i in l:
+        if type(i) != str:
+            raise ValueError('expected list of strings')
+
+def flatten_str_2l(l):
+    '''Flatten a list of either strings or lists of strings'''
+
+    result = []
+
+    for i in l:
+        t = type(i)
+        if t == str:
+            result.append(i)
+        elif t in [list, tuple]:
+            check_str_list(i)
+            result.extend(i)
+
+    return result
+
+def read_table(con, meta, where=None):
+    '''Generate a list of dictionaries from the meta table.
+    Column names may be tuples'''
+    result = []
+    columns = flatten_str_2l(meta['columns'].keys())
+    sql = 'SELECT %s FROM %s' % (', '.join(columns), meta['table'])
+
+    if where:
+        sql = sql + ' WHERE ' + where
+
+    cur = con.cursor()
+
+    cur.execute(sql)
+
+    for row in cur:
+        rv = { 'type': meta['typename'] }
+        i = 0
+        for cnames, emit in meta['columns'].iteritems():
+            if type(cnames) == str:
+                kv = emit(con, cnames, row[i])
+                i = i + 1
+            else:
+                l = len(cnames)
+                kv = emit(con, cnames, row[i:i+l])
+                i = i + l
+
+            if kv is None:
+                raise RuntimeError(
+                    'emit returned None for %s.%s' %
+                        (meta['table'], cnames))
+
+            k, v = kv
+
+            if truthy(v):
+                rv[k] = v
+
+        result.append(rv)
+
+    return result
+
+def read_value_list(con):
+    values = {}
+
+    value_list = read_table(con, cm_value_list)
+    # print(value_list)
+
+    for v in value_list:
+        _id = v['id_value']
+        category = v['list_name']
+        key = v['tri']
+        value = v['value']
+
+        # if not values.has_key(category):
+        #     values[category] = {}
+        #
+        # values[category][key] = value
+
+        values[_id] = value
+
+    return values
+
+def emit(name, _type=None):
+    def fn(con, column, value):
+        if _type is None:
+            return name, value
+        return name, _type(value)
+    return fn
+
+def emit_vl(name):
+    def fn(con, column, value):
+        return name, cm_values[value]
+    return fn
+
+def emit_urls(con, columns, values):
+    rv = None
+    names = ['personal', 'myspace', 'facebook']
+    for n, v in zip(names, values):
+        if v:
+            if rv is None:
+                rv = {}
+            rv[n] = v
+
+    return 'urls', rv
+
+def emit_interpretation(con, colums, values):
+    return [cm_values[v] for v in values]
+
+def emit_media(con, column, value):
+    print(value)
+    media = read_table(con, cm_medias, 'id_comedien = %d' % value)
+
+    for m in media:
+        del m['id_media']
+
+    return 'recordings', media
+
+cm_value_list = {
+    'table': 'cm_value_list',
+    'typename': 'values',
+    'columns': {
+        'id_value': emit('id_value'),
+        'list_name': emit('list_name'),
+        'tri': emit('tri'),
+        'value': emit('value')
+    }
+}
+
+cm_sons = {
+    'table': 'cm_sons',
+    'typename': 'attributes',
+    'columns': {
+        'qf_diffusion': emit('diffusion'),
+        'qf_accent': emit('accent'),
+        'qf_age': emit('age'),
+        'qf_cartoon': emit_vl('cartoon'),
+        'qf_doublage': emit('double'),
+        ('qf_interpretation1', 'qf_interpretation2', 'qf_interpretation3'): emit_interpretation,
+        'qf_imitation': emit_vl('imitation'),
+        'qf_langue': emit_vl('language'),
+        'qf_personnage': emit_vl('personality'),
+        'qf_timbre': emit_vl('timbre'),
+        'qf_chante': emit_vl('genre'),
+        'qf_genre': emit('gender'),
+        'timestamp_creation': emit('created'),
+        'timestamp_modification': emit('modified')
+    }
+}
+
+cm_medias = {
+    'table': 'cm_medias',
+    'typename': 'recording',
+    'columns': {
+        'id_media': emit('id_media'),
+        'intitule': emit('title'),
+        'filename': emit('filename'),
+        'original_filename': emit('original')
+    }
+}
+
+cm_comediens = {
+    'table': 'cm_comediens',
+    'typename': 'actor',
+    'columns': {
+        'id_comedien': emit_media,
+        'comedien_ft': emit('name'),
+        'comedien_perso_adresse': emit('address'),
+        'comedien_perso_email': emit('email'),
+        'infos_cv': emit('cv'),
+        'infos_news': emit('news'),
+        ('site_perso', 'lien_myspace', 'lien_facebook'): emit_urls,
+    }
+}
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='export castingmachine database')
+    parser.add_argument('-e', '--export', default=False, action='store_true')
+    parser.add_argument('-u', '--user', default='pbenso')
+    parser.add_argument('-s', '--server', default='139.162.157.175')
+    parser.add_argument('-i', '--index',
+                        default='cm_import',
+                        help="index name (default is 'cm_import')")
+
+    args = parser.parse_args()
+
+    con = mdb.connect(
+        args.server, args.user,
+        get_keyring().get_password('studiotime', args.user),
+        'cm');
+
+    cm_values = read_value_list(con)
+    print(cm_values)
+
+    actors = read_table(con, cm_comediens)
+
+    print(actors[0])
+
+    if args.export:
+        indexname = args.index
+
+        print('uploading to algolia as ', indexname)
+
+        pw = get_keyring().get_password('studiotime', 'algolia')
+        # print(pw)
+
+        algo = algoliasearch.Client('OYKQJYHGCO', pw)
+
+        algo.delete_index(indexname)
+
+        index = algo.init_index(indexname)
+        index.add_objects(actors)
